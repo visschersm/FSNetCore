910c51f65a3e39fc6ef26d8ab3e0b84f3311b7f3
Mark Visschers <mark.visschers@connecthing.tech>
2021-05-30 15:19:26 +0200

    first commit

---
+++ src/Library/Library.fs
@@ -0,0 +1,7 @@
+﻿module Library
+
+open Newtonsoft.Json
+
+let getJsonNetJson value = 
+    let json = JsonConvert.SerializeObject(value)
+    $"I used to be {value} but now i'm {json} thanks to JSON.NET!"
